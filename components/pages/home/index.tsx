bd1ce5b637f0adaf87b8853fad5ec7547b4239e4
robnsiov <robnsiov@gmail.com>
2023-07-23 18:07:54 +0330

    add home page

--- components/pages/home/index.tsx
+++ components/pages/home/index.tsx
@@ -1,4 +1,31 @@
+import { PlayCircle } from "iconsax-react";
+import Image from "next/image";
+
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="w-full h-screen overflow-hidden">
+        <div className="absolute inset-0 z-10 flex justify-center items-center">
+          <PlayCircle
+            className="text-white cursor-pointer md:hidden"
+            size="206"
+          />
+          <PlayCircle
+            className="text-white cursor-pointer hidden md:inline-block"
+            size="106"
+          />
+        </div>
+        <div className="w-full h-full relative blur-sm scale-110">
+          <Image
+            src={"/images/Amir Tataloo - Asheghi Nagaeidam.jpg"}
+            width="2000"
+            height="2000"
+            alt="cover"
+            className="w-full h-full object-cover object-top"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 export default Home;
