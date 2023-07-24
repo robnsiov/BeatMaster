30632ead36d6e3ae4b2cf3bd7a30146f7c10fbc8
robnsiov <robnsiov@gmail.com>
2023-07-24 18:38:24 +0330

    fix the width of the artist section

--- components/pages/music/artist/index.tsx
+++ components/pages/music/artist/index.tsx
@@ -2,22 +2,24 @@ const Artist = () => {
   return (
     <>
       <div
-        className="flex justify-start items-start flex-col text-white
-             max-w-[30%] relative z-[60] opacity-0 scale-110 blur-md
-              fade-scale-artist xl:max-w-none rtl xl:ltr xl:text-center"
+        className="flex justify-end items-start  text-white
+             w-[30%] relative z-[60] opacity-0 scale-110 blur-md
+              fade-scale-artist xl:w-none rtl xl:ltr xl:text-center"
       >
-        <h1
-          className="uppercase text-3xl font-medium mb-1 
+        <div className="flex justify-start items-start flex-col">
+          <h1
+            className="uppercase text-3xl font-medium mb-1 
             whitespace-nowrap md:whitespace-normal md:w-full md:text-xl"
-        >
-          Asheghi Nagaeidam
-        </h1>
-        <h2
-          className="uppercase tracking-[10px] w-full 
+          >
+            Asheghi Nagaeidam
+          </h1>
+          <h2
+            className="uppercase tracking-[10px] w-full 
             text-left xl:text-center md:text-[13px]"
-        >
-          Amir Tataloo
-        </h2>
+          >
+            Amir Tataloo
+          </h2>
+        </div>
       </div>
     </>
   );
