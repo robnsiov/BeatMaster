eb9ef4042b274089d6a67237fd20cda344eaaa92
robnsiov <robnsiov@gmail.com>
2023-07-26 20:50:18 +0330

    add music slowly

--- components/pages/music/player/index.tsx
+++ components/pages/music/player/index.tsx
@@ -4,9 +4,17 @@ import "react-h5-audio-player/lib/styles.css";
 import "./index.css";
 import { Next, Pause, Play, Previous } from "iconsax-react";
 import usePlayer from "./use";
+import { useEffect, useRef } from "react";
+import { useTimeout } from "@mantine/hooks";
 
 const Player = ({ url }: { url: string }) => {
-  const { next, prev } = usePlayer();
+  const playerRef = useRef(null);
+
+  // @ts-ignore
+  const audio: HTMLAudioElement = playerRef.current?.audio?.current;
+
+  const { next, prev, isPlay, setIsPlay, pause, play } = usePlayer(audio);
+
   return (
     <>
       <div className="w-full player relative bottom-1">
@@ -16,10 +24,25 @@ const Player = ({ url }: { url: string }) => {
         >
           <Previous
             className="active:scale-90 
-          transition-all duration-200 cursor-pointer"
+            transition-all duration-200 cursor-pointer"
             size="20"
             onClick={prev}
           />
+          <div className="text-white cursor-pointer">
+            {isPlay ? (
+              <Pause
+                className="active:scale-90 transition-all duration-200"
+                size="20"
+                onClick={pause}
+              />
+            ) : (
+              <Play
+                className="active:scale-90 transition-all duration-200"
+                size="20"
+                onClick={play}
+              />
+            )}
+          </div>
           <Next
             className="active:scale-90 
           transition-all duration-200 cursor-pointer"
@@ -30,24 +53,17 @@ const Player = ({ url }: { url: string }) => {
         <AudioPlayer
           src={url}
           autoPlay
+          ref={playerRef}
+          onPause={() => setIsPlay(false)}
+          onPlay={() => setIsPlay(true)}
           customIcons={{
             loop: <></>,
             forward: <></>,
             loopOff: <></>,
             next: <></>,
             volume: <></>,
-            pause: (
-              <Pause
-                className="text-white active:scale-90 transition-all duration-200"
-                size="20"
-              />
-            ),
-            play: (
-              <Play
-                className="text-white active:scale-90 transition-all duration-200"
-                size="20"
-              />
-            ),
+            pause: <></>,
+            play: <></>,
             previous: <></>,
             rewind: <></>,
             volumeMute: <></>,
--- components/pages/music/player/use.tsx
+++ components/pages/music/player/use.tsx
@@ -4,12 +4,16 @@ import getMusic from "@/utils/get-music";
 import { useQuery } from "@tanstack/react-query";
 import { useRecoilState } from "recoil";
 import { useState, useEffect } from "react";
-import { queryClient } from "@/components/containers/react-query";
+import { UsePlayerImpl } from "./types";
+import { useTimeout } from "@mantine/hooks";
 
-const usePlayer = () => {
+const usePlayer = (audio: UsePlayerImpl) => {
   const [_, setIsNextMusic] = useRecoilState(isNextMusicState);
   const [fetchNextMusic, setFetchNextMusic] = useState(false);
   const [fetchPrevMusic, setFetchPrevMusic] = useState(false);
+  const [isPlay, setIsPlay] = useState(false);
+  const { start: startPause } = useTimeout(() => audio.pause(), 1000);
+
   const { isFetching: nextFetching } = useQuery({
     queryKey: ["music"],
     queryFn: () => getMusic<MusicApiImpl>("http://localhost:5000/next"),
@@ -50,7 +54,62 @@ const usePlayer = () => {
   const prev = () => {
     getPrevMusic();
   };
-  return { next, prev };
+
+  let decInterval: NodeJS.Timer;
+  let incInterval: NodeJS.Timer;
+
+  const clearIntervals = () => {
+    clearInterval(decInterval);
+    clearInterval(incInterval);
+  };
+
+  const decreseVolume = () => {
+    clearIntervals();
+    decInterval = setInterval(() => {
+      const volume = +audio.volume.toFixed(1);
+      console.log(volume);
+      if (volume >= 0.1) audio.volume = volume - 0.1;
+      else clearInterval(decInterval);
+    }, 100);
+  };
+  const increaseVolume = () => {
+    clearIntervals();
+    incInterval = setInterval(() => {
+      const volume = +audio.volume.toFixed(1);
+      console.log(volume);
+      if (volume <= 0.9) audio.volume = volume + 0.1;
+      else clearInterval(incInterval);
+    }, 100);
+  };
+
+  const pause = () => {
+    setIsPlay(false);
+    decreseVolume();
+    let timer;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      audio.pause();
+    }, 1000);
+  };
+  const play = () => {
+    setIsPlay(true);
+    increaseVolume();
+    let timer;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      audio.play();
+    }, 1000);
+  };
+
+  return {
+    next,
+    prev,
+    isPlay,
+    setIsPlay,
+    decreseVolume,
+    pause,
+    play,
+  };
 };
 
 export default usePlayer;
